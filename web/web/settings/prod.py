04cc85595c62877d5e60f079a6467117812e15d3
Timothy Cleveland <timothy.j.cleveland1@gmail.com>
2020-08-20 13:21:21 -0400

    added staticfiles

--- web/web/settings/prod.py
+++ web/web/settings/prod.py
@@ -6,8 +6,6 @@ DEBUG = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../static'),
 )
---
+++ web/web/staticfiles/src/components/NextUp.vue
@@ -0,0 +1,79 @@
+<template>
+	<div id="main-container">
+		<div class="title">Next up</div>
+		<a id="next-up" :href="href" target="_blank" @click="addLink">
+			<div>
+				Session {{ sessionNum }}
+			</div>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NextUp',
+	computed: {
+		href() {
+			return this.$store.getters.nextUp.href
+		},
+		sessionNum() {
+			return this.$store.getters.nextUp.sessionNum
+		},
+	},
+	methods: {
+		addLink() {
+			if (this.sessionNum < 30) {
+				const items = localStorage.getItem('visitedLinks')
+				if (items) {
+					let obj = JSON.parse(items)
+					obj.push(this.sessionNum)
+					localStorage.setItem('visitedLinks', JSON.stringify(obj))
+				} else {
+					localStorage.setItem('visitedLinks', JSON.stringify([this.sessionNum]))
+				}
+				this.updateVisitedArr()
+			} else if (parseInt(this.sessionNum) === 30) {
+				localStorage.setItem('visitedLinks', JSON.stringify([]))
+				this.updateVisitedArr()
+			}
+		},
+		updateVisitedArr() {
+			let timeout = window.setTimeout(() => {
+				this.$store.dispatch('setVisitedArr')
+				this.$store.dispatch('setLastSessionDate')
+				window.clearTimeout(timeout)
+			}, 100)
+		}
+	},
+}
+</script>
+
+<style scoped>
+	#main-container {
+		display: flex;
+		margin-top: 100px;
+		flex: 1;
+		flex-direction: column;
+		align-items: center;
+		
+	}
+	
+	.title {
+		font-size: 60px;
+		width: auto;
+		transition: color 250ms ease;
+	}
+	
+	#next-up {
+		text-decoration: none;
+		font-size: 40px;
+		width: auto;
+		transition: 250ms all ease;
+		color: var(--color-light);
+		
+	}
+	
+	#next-up:hover {
+		color: var(--color-dark);
+	}
+</style>
